be5ada076a16626da93f177d802c8a738fc70cac
Sarah Scheffler <sscheffler@hmc.edu>
2017-04-14 16:23:47 -0400

    Hello World

---
+++ hello.rs
@@ -0,0 +1,9 @@
+// 4/14/17
+// Compile with rustc, run with ./
+//
+// These are comments I guess.
+fn main() {
+    println!("Hello, World!")
+    // Whoa, there's a lot going on here: what's the !, why
+    // are we allowed to use println, ...
+}
